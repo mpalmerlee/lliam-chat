d7e58dc63fb1ac91f02bb04300885ea02627e29a
Matt Palmerlee <mpalmerlee@gmail.com>
2024-02-27 17:53:44 -0800

    lliam-chat

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <div class="about">
-      <h1>About Ollama Web</h1>
+      <h1>About Lliam Chat</h1>
       <p class="text-center">
-        Ollama Web is a simple web application that uses the Ollama API to
+        Lliam is your personal Large Language Intelligent Assistant Model.
+      </p>
+      <p class="text-center">
+        Lliam Chat is a simple web application that uses the Ollama API to
         provide a chat interface for users to interact with Ollama models.
       </p>
       <div class="pt-6">
         <ul>
           <li>
             <a href="https://github.com/mpalmerlee/ollama-web"
-              >Ollama Web Github</a
+              >Lliam Chat Github</a
             >
           </li>
           <li><a href="https://ollama.com/">Ollama</a></li>
